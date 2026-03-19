c28373338f098ab057baf232844e3c9597fe0458
ErickooKun <ericnonger@gmail.com>
2026-03-19 14:04:01 +0900

    Working on float number system

---
+++ Assets/_Project/Prefabs/World/FloatingNumber/FloatingNumber.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+using System;
+
+namespace FloatingNumber
+{
+    public class FloatingNumber : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text text;
+    }
+}
---
+++ Assets/_Project/Scripts/Core/Event Bus/CombatEvents.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System;
+
+public static class CombatEvents
+{
+    public static event Action<int, Vector3> OnFloatingNumberSpawnRequested;
+    
+    // Called from requesters to spawn a floating number
+    public static void RequestingFloatingNumberSpawn(int amount, Vector3 position)
+    {
+        OnFloatingNumberSpawnRequested?.Invoke(amount, position);
+    }
+}
